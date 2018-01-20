de97e43ff3cbc41abbe75a2fda330fee45dc928b
Mac User <macuser@Macs-MacBook-Pro.local>
2018-01-20 13:25:34 -0800

    add db in progress, 2nd attempt

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ re: code below for pg
 const pg = require('pg');
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
