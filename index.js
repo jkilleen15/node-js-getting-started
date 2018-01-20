978629c8d1dc48d855faa8b6736225623f6f6752
Mac User <macuser@Macs-MacBook-Pro.local>
2018-01-20 11:34:07 -0800

    Demo

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const express = require('express')
-const path = require('path')
+const cool = require('cool-ascii-faces');
+const express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function (request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+/*
+// from previous code, beginning after express =
+// this code may be newer, but changing to
+ heroku tutorial format (pushing local changes)
+
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,3 +38,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+*/
